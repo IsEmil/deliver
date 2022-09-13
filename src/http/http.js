b799cbb098d288efd994ad6f4dff5d2f062391a3
Emil <emilkeserovic@Outlook.com>
2022-09-13 22:46:35 +0200

    Completed Http

---
+++ src/http/http.js
@@ -0,0 +1,29 @@
+const messenger = require("messenger");
+const express = require("express");
+const helmet = require("helmet");
+
+const config = require("../config.js");
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("X-Powered-By", "Deliver");
+    next();
+});
+
+app.get("/", (_, res) => {
+    res.status(200).send("OK");
+});
+
+app.get("/roblox/pay", require("./routes/roblox.pay.js"));
+
+app.get("*", (_, res) => {
+    res.status(404).send("Not Found");
+});
+
+module.exports = () => {
+    app.listen(config.port, () => {
+        console.log(`[Deliver]: Listening on port ${config.port}`);
+    });
+}
