3854dddd554b7e8145d5825a502c0cfd7403b97c
Emil <emilkeserovic@Outlook.com>
2022-09-13 21:57:24 +0200

    Added global_slash in config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 module.exports = exports = {
-
     token: process.env.TOKEN,
     mongo: process.env.MONGODB,
     port: 3000,
     loader: 0,
     guild: "1015369415879032964",
+    global_slash: false, 
 
     embeds: {
         colors: {
