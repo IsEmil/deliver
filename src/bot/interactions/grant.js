116e5fab942ba889c98286d88846764f37ca5f5c
Emil <emilkeserovic@Outlook.com>
2022-09-13 16:16:31 +0200

    Completed Grant

--- src/bot/interactions/grant.js
+++ src/bot/interactions/grant.js
@@ -9,7 +9,6 @@ const {
     ModalBuilder,
     Interaction,
     GuildMember,
-    Client,
     EmbedBuilder,
     TextInputBuilder,
     TextInputStyle,
@@ -21,6 +20,7 @@ const fs = require('fs');
 
 const config = require('../../config.js');
 const Hub = require("../../database/Hub");
+const Client = require("../../database/Client");
 const Product = require("../../database/Product");
 
 /**
@@ -77,14 +77,64 @@ async function run(interaction, member) {
         });
     }
 
-    await Product.deleteOne({
-        _id: productRecord._id
+    const match = clientRecord.purchases.find((purchase) => {
+        const purchaseProduct = purchase.product.toString();
+
+        return purchaseProduct === productRecord.toString();
     });
+
+    if (match) {
+        let Embed = new EmbedBuilder()
+            .setTitle("Error")
+            .setDescription("Client already owns specified product")
+            .setColor(config.embeds.colors.danger);
+        return await interaction.reply({
+            embeds: [Embed]
+        });
+    }
+
+    const purchaseObject = clientRecord.purchases.create({
+        product: productRecord._id,
+        hub: hubRecord._id,
+    });
+
+    clientRecord.purchases.push(purchaseObject);
+    await clientRecord.save();
 
     console.log(`[Deliver]: Product granted "${productName}" to "${clientRecord._id}" in "${hubRecord._id}"`);
 
+    let GrantEmbed = new EmbedBuilder()
+        .setTitle("Product Granted")
+        .addFields({ name: "Product", value: productName })
+        .setDescription("You have been granted a product. Any product information/files will be sent below shortly.")
+        .setColor(config.embeds.colors.default);
+    await user.send({
+        embeds: [GrantEmbed]
+    });
+
+    switch (productRecord.file.type) {
+        case "1":
+            let MessageEmbed = new EmbedBuilder()
+                .setTitle("Product Message")
+                .setDescription(`For the product: ${product.name}\n\`\`\`${product.file.target}\`\`\``)
+                .setColor(config.embeds.colors.default);
+            user.send({
+                embeds: [MessageEmbed]
+            });
+            break;
+        case "2":
+            let FileEmbed = new EmbedBuilder()
+                .setTitle("Product Link")
+                .setDescription(`For the product: ${product.name}\n\n[${product.file.target}](${product.file.target})`)
+                .setColor(config.embeds.colors.default);
+            user.send({
+                embeds: [FileEmbed]
+            });
+            break;
+    }
+
     let Embed = new EmbedBuilder()
-        .setTitle("Granted Successful")
+        .setTitle("Granted")
         .setDescription("You have successfully granted a user the product.")
         .setColor(config.embeds.colors.success);
     return await interaction.reply({
